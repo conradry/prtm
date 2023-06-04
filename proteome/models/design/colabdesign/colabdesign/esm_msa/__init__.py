470ecd7a1d569d57edbca4d89825e3c1c0c2334b
conradry <conradry71@gmail.com>
2023-06-04 15:53:24 -0700

    remove colabdesign readme files

--- proteome/models/design/colabdesign/colabdesign/esm_msa/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Levinthal, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-from . import pretrained  # noqa
-from . import config
-from .data import Alphabet, BatchConverter, FastaBatchedDataset  # noqa
-from .model import MSATransformer, RunModel  # noqa
