8fdcaa76b2c51991572ad52aa363db294ba1dfdc
conradry <conradry71@gmail.com>
2023-07-30 23:26:50 +0000

    consolidate diffusion code

--- proteome/models/design/genie/diffusion.py
+++ proteome/models/design/genie/diffusion.py
@@ -1,8 +1,102 @@
+import math
+from abc import ABC, abstractmethod
+
 import torch
-from proteome.models.design.genie.diffusion.diffusion import Diffusion
+from tqdm import tqdm
+
+from proteome.models.design.genie import config
+from proteome.models.design.genie.model.model import Denoiser
 from proteome.models.design.genie.diffusion.schedule import get_betas
 from proteome.models.design.genie.utils.affine_utils import T
 from proteome.models.design.genie.utils.geo_utils import compute_frenet_frames
+
+
+def get_betas(n_timestep, schedule):
+    if schedule == "linear":
+        return linear_beta_schedule(n_timestep)
+    elif schedule == "cosine":
+        return cosine_beta_schedule(n_timestep)
+    else:
+        print("Invalid schedule: {}".format(schedule))
+        exit(0)
+
+
+def linear_beta_schedule(n_timestep, start=0.0001, end=0.02):
+    return torch.linspace(start, end, n_timestep)
+
+
+def cosine_beta_schedule(n_timestep):
+    steps = n_timestep + 1
+    x = torch.linspace(0, n_timestep, steps)
+    alphas_cumprod = torch.cos((x / steps) * math.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0, 0.999)
+
+
+class Diffusion(ABC):
+    def __init__(self, cfg: config.GenieConfig):
+        super(Diffusion, self).__init__()
+
+        self.cfg = cfg
+
+        self.model = Denoiser(
+            self.config.model, n_timestep=self.config.diffusion.n_timestep
+        )
+
+        self.setup = False
+
+    @abstractmethod
+    def setup_schedule(self):
+        """
+        Set up variance schedule and precompute its corresponding terms.
+        """
+        raise NotImplemented
+
+    @abstractmethod
+    def transform(self, batch):
+        """
+        Transform batch data from data pipeline into the desired format
+
+        Input:
+                batch - coordinates from data pipeline (shape: b x (n_res * 3))
+
+        Output: frames (shape: b x n_res)
+        """
+        raise NotImplemented
+
+    @abstractmethod
+    def sample_timesteps(self, num_samples):
+        raise NotImplemented
+
+    @abstractmethod
+    def sample_frames(self, mask):
+        raise NotImplemented
+
+    @abstractmethod
+    def q(self, t0, s, mask):
+        raise NotImplemented
+
+    @abstractmethod
+    def p(self, ts, s, mask):
+        raise NotImplemented
+
+    def p_sample_loop(self, mask, verbose=True):
+        if not self.setup:
+            self.setup_schedule()
+            self.setup = True
+        ts = self.sample_frames(mask)
+        ts_seq = [ts]
+        for i in tqdm(
+            reversed(range(self.config.diffusion["n_timestep"])),
+            desc="sampling loop time step",
+            total=self.config.diffusion["n_timestep"],
+            disable=not verbose,
+        ):
+            s = torch.Tensor([i] * mask.shape[0]).long().to(self.device)
+            ts = self.p(ts, s, mask)
+            ts_seq.append(ts)
+        return ts_seq
 
 
 class Genie(Diffusion):
