e26c7b12ed4f9566c043adee37579878412f4ccc
conradry <conradry71@gmail.com>
2023-08-24 02:55:58 +0000

    initial notebook and all I thought I committed last time

--- proteome/models/design/eigenfold/polymer.py
+++ proteome/models/design/eigenfold/polymer.py
@@ -1,10 +1,14 @@
-from .sde import HarmonicSDE
 import numpy as np
+
+from proteome.models.design.eigenfold.sde import HarmonicSDE
+
 
 class PolymerSDE(HarmonicSDE):
     def __init__(self, N, a, b):
-        super(PolymerSDE, self).__init__(N=N, 
-            edges=zip(np.arange(N-1), np.arange(1, N)),
-            antiedges=zip(np.arange(N-2), np.arange(2, N)),
-        a=a, b=b)
-    
+        super(PolymerSDE, self).__init__(
+            N=N,
+            edges=zip(np.arange(N - 1), np.arange(1, N)),
+            antiedges=zip(np.arange(N - 2), np.arange(2, N)),
+            a=a,
+            b=b,
+        )
--- proteome/models/design/eigenfold/score_model.py
+++ proteome/models/design/eigenfold/score_model.py
@@ -1,13 +1,18 @@
-from .resi_score_model import ResiLevelTensorProductScoreModel
 import torch
+
+from proteome.models.design.eigenfold.resi_score_model import (
+    ResiLevelTensorProductScoreModel,
+)
+
 
 class ScoreModel(torch.nn.Module):
     def __init__(self, args):
         super(ScoreModel, self).__init__()
         self.enn = ResiLevelTensorProductScoreModel(args)
-        
+
     def forward(self, data):
         return self.enn(data)
+
 
 def get_model(args):
     return ScoreModel(args)
