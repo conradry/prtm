7a95a282a24c788cb9fa682d6693e671ca7b21e3
conradry <conradry71@gmail.com>
2023-08-26 03:02:20 +0000

    remove all scripts

--- scripts/models/design/foldingdiff/data/download_cath.sh
+++
@@ -1,9 +0,0 @@
-# cd into data directory and execute shell script
-wget -P cath ftp://orengoftp.biochem.ucl.ac.uk/cath/releases/latest-release/non-redundant-data-sets/cath-dataset-nonredundant-S40.pdb.tgz
-
-# Check md5sums
-md5sum -c cath_checksums.md5
-
-# cd into the cath directory and untar the file
-cd cath
-tar -xzf cath-dataset-nonredundant-S40.pdb.tgz
