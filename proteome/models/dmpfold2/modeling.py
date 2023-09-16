fix hetatm bug, add bfactors 100, fix test, silent pyrosetta

proteome/models/dmpfold2/modeling.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List, Tuple
 import numpy as np
 import torch
 import torch.nn.functional as F
+
 from proteome import protein
 from proteome.models.dmpfold2 import config, model, utils
 from proteome.query.pipeline import QueryPipelines
@@ -65,9 +66,7 @@ class DMPFoldForFolding:
         for url in weights_urls:
             name = url.split("/")[-1]
             state_dict.update(
-                torch.hub.load_state_dict_from_url(
-                    url, file_name=f"dmpfold2_{name}.pt"
-                )
+                torch.hub.load_state_dict_from_url(url, file_name=f"dmpfold2_{name}.pt")
             )
         msg = self.model.load_state_dict(state_dict, strict=True)
 
@@ -145,14 +144,14 @@ class DMPFoldForFolding:
             )
 
         xyz = xyz.view(-1, L, 5, 3)[0].cpu().numpy()
-        confs = confs[0].cpu().numpy()
+        confs = 100 * confs[0].cpu().numpy()
 
         predicted_protein = protein.Protein5(
             atom_positions=xyz,
             aatype=feature_dict["msa"][0].cpu().numpy(),
             atom_mask=np.ones_like(xyz)[..., 0],
             residue_index=np.arange(L) + 1,
-            b_factors=np.zeros_like(xyz)[..., 0],  # no b_factors
+            b_factors=confs[:, None].repeat(5, axis=1),
             chain_index=np.zeros(L, dtype=np.int32),
         )
         confidence = float(confs.mean())

proteome/models/omegafold/modeling.py
@@ -147,9 +147,7 @@ class OmegaFoldForFolding:
             .numpy(),  # first msa is sequence
             atom_mask=res["final_atom_mask"].cpu().numpy(),
             residue_index=np.arange(len(sequence)) + 1,
-            b_factors=np.zeros(
-                tuple(res["final_atom_positions"].shape[:2])
-            ),  # no lddt predicted
+            b_factors=100 * res["confidence"].cpu().numpy()[:, None].repeat(14, axis=1),
             chain_index=np.zeros(len(sequence), dtype=np.int32),
         )
 

proteome/models/rfdiffusion/modeling.py
@@ -211,22 +211,25 @@ class RFDiffusionForStructureDesign:
 
         # Get the output structure
         final_seq = seq_stack[-1]
-
+        final_plddt = plddt_stack[-1]
         # Output glycines, except for motif region
         final_seq = torch.where(
             torch.argmax(seq_init, dim=-1) == 21, 7, torch.argmax(seq_init, dim=-1)
         )  # 7 is glycine
 
-        bfacts = torch.ones_like(final_seq.squeeze())
-        # make bfact=0 for diffused coordinates
-        bfacts[torch.where(torch.argmax(seq_init, dim=-1) == 21, True, False)] = 0
+        # b_factors will be 100 for all residues that were not diffused
+        bfacts = 100 * np.ones((len(final_seq),))
+        diffused_mask = (
+            torch.where(torch.argmax(seq_init, dim=-1) != 21, False, True).cpu().numpy()
+        )
+        bfacts[diffused_mask] = 100 * final_plddt.cpu().numpy()[diffused_mask]
 
         result = protein.Protein14(
             atom_positions=denoised_xyz_stack[-1].numpy(),
             aatype=final_seq.numpy(),
-            atom_mask=np.ones(denoised_xyz_stack[-1].shape[:2], dtype=np.bool_),
+            atom_mask=protein.ideal_atom14_mask(final_seq.numpy()),
             residue_index=np.arange(1, len(final_seq) + 1, dtype=np.int32),
-            b_factors=bfacts[:, None].numpy().repeat(14, axis=-1),
+            b_factors=bfacts[:, None].repeat(14, axis=-1),
             chain_index=np.array(
                 [protein.PDB_CHAIN_IDS.index(char) for char in sampler.chain_idx]
             ),

proteome/models/rosettafold/modeling.py
@@ -148,7 +148,7 @@ class RoseTTAFoldForFolding:
             aatype=feature_dict["seq"][0].cpu().numpy(),
             atom_mask=np.ones_like(xyzo)[..., 0],
             residue_index=feature_dict["idx"][0].cpu().numpy() + 1,
-            b_factors=lddt[0].cpu().numpy()[:, None].repeat(4, axis=1),
+            b_factors=100 * lddt[0].cpu().numpy()[:, None].repeat(4, axis=1),
             chain_index=np.zeros((seqlen,), dtype=np.int32),
         )
         mean_plddt = float(lddt.mean())

proteome/models/se3_diffusion/modeling.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, Optional, Tuple
 
 import numpy as np
 import torch
+
 from proteome import protein
 from proteome.constants import residue_constants
 from proteome.models.se3_diffusion import config
@@ -87,7 +88,7 @@ class SE3DiffusionForStructureDesign:
             aatype=np.array(length * [residue_constants.restype_order_with_x["G"]]),
             atom_mask=np.ones((length, num_atoms)),
             residue_index=np.arange(0, length),
-            b_factors=np.ones((length, num_atoms)),
+            b_factors=np.zeros((length, num_atoms)),
             chain_index=np.zeros((length,), dtype=np.int32),
         )
 

proteome/protein.py
@@ -18,6 +18,7 @@ from __future__ import annotations
 
 import io
 import string
+from collections import namedtuple
 from typing import Any, Dict, Mapping, Optional, Sequence, Union
 
 import modelcif
@@ -33,6 +34,16 @@ from Bio.PDB import PDBParser
 from Bio.PDB.Structure import Structure
 
 from proteome.constants import residue_constants
+from proteome.visual import view_protein_with_bfactors
+
+try:
+    import pyrosetta
+    from pyrosetta.rosetta.core.import_pose import pose_from_pdbstring
+    from pyrosetta.rosetta.core.pose import Pose
+
+    pyrosetta.init(silent=True, extra_options="-mute all")
+except:
+    pass
 
 FeatureDict = Mapping[str, np.ndarray]
 ModelOutput = Mapping[str, Any]  # Is a nested dict.
@@ -316,6 +327,8 @@ class ProteinBase:
                     prot_dict[field] = torch.from_numpy(v).long()
                 elif np.issubdtype(v.dtype, np.bool_):
                     prot_dict[field] = torch.from_numpy(v).bool()
+                else:
+                    prot_dict[field] = v
             elif isinstance(v, torch.Tensor):
                 # If already a tensor, do nothing
                 prot_dict[field] = v
@@ -640,11 +653,8 @@ class ProteinBase:
 
     def to_rosetta_pose(self):  # can't type hint conditional import
         """Converts a protein to a PyRosetta pose."""
-        try:
-            from pyrosetta.rosetta.core.import_pose import pose_from_pdbstring
-            from pyrosetta.rosetta.core.pose import Pose
-        except:
-            raise ImportError("PyRosetta is not installed")
+        if "pyrosetta" not in globals():
+            raise Exception("PyRosetta is not imported.")
 
         pose = Pose()
         pdb_str = self.to_pdb()
@@ -709,6 +719,8 @@ class ProteinBase:
             parents=protein.parents,
             parents_chain_index=protein.parents_chain_index,
             remark=protein.remark,
+            hetatom_positions=protein.hetatom_positions,
+            hetatom_names=protein.hetatom_names,
         )
 
     def _crop_n_atoms(self, n: int) -> Dict[str, np.ndarray]:
@@ -728,11 +740,13 @@ class ProteinBase:
             parents=protein.parents,
             parents_chain_index=protein.parents_chain_index,
             remark=protein.remark,
+            hetatom_positions=protein.hetatom_positions,
+            hetatom_names=protein.hetatom_names,
         )
 
     def to_protein37(self) -> Protein37:
         raise NotImplementedError
-    
+
     def to_protein27(self) -> Protein27:
         raise NotImplementedError
 
@@ -764,6 +778,29 @@ class ProteinBase:
             parents=self.parents,
             parents_chain_index=self.parents_chain_index,
             remark=self.remark,
+            hetatom_positions=self.hetatom_positions,
+            hetatom_names=self.hetatom_names,
+        )
+
+    def get_chain(self, chain_id: str) -> ProteinBase:
+        assert chain_id in PDB_CHAIN_IDS, f"Invalid chain_id: {chain_id}"
+        chain_index = PDB_CHAIN_IDS.index(chain_id)
+        # Get the mask for indices in this chain
+        chain_mask = self.chain_index == chain_index
+        # Create a new Protein instance that only includes the
+        # relevant chain
+        return type(self)(
+            atom_positions=self.atom_positions[chain_mask],
+            atom_mask=self.atom_mask[chain_mask],
+            aatype=self.aatype[chain_mask],
+            residue_index=self.residue_index[chain_mask],
+            chain_index=self.chain_index[chain_mask],
+            b_factors=self.b_factors[chain_mask],
+            parents=self.parents,
+            parents_chain_index=self.parents_chain_index,
+            remark=self.remark,
+            hetatom_positions=self.hetatom_positions,
+            hetatom_names=self.hetatom_names,
         )
 
     def sequence(self, chain_id: str) -> str:
@@ -771,6 +808,23 @@ class ProteinBase:
         assert chain_id in PDB_CHAIN_IDS, f"Invalid chain_id: {chain_id}"
         chain_aatype = self.aatype[self.chain_index == PDB_CHAIN_IDS.index(chain_id)]
         return "".join(residue_constants.restypes[a] for a in chain_aatype)
+
+    def show(self, cmap: str = "viridis", show_sidechains: bool = True):
+        return view_protein_with_bfactors(
+            self, cmap=cmap, show_sidechains=show_sidechains
+        )
+
+    @property
+    def shape(self):
+        # Get the shape as a named tuple of (num_residues, num_atoms, num_chains)
+        shape_tuple = namedtuple(
+            "ProteinShape", ["num_residues", "num_atoms", "num_chains"]
+        )
+        return shape_tuple(
+            num_residues=self.aatype.shape[0],
+            num_atoms=self.atom_positions.shape[1],
+            num_chains=len(np.unique(self.chain_index)),
+        )
 
 
 class Protein37(ProteinBase):
@@ -816,6 +870,8 @@ class Protein37(ProteinBase):
             parents=protein37.parents,
             parents_chain_index=protein37.parents_chain_index,
             remark=protein37.remark,
+            hetatom_positions=protein37.hetatom_positions,
+            hetatom_names=protein37.hetatom_names,
         )
 
     def to_protein27(self) -> Protein27:
@@ -884,6 +940,8 @@ class Protein14(ProteinBase):
             parents=protein14.parents,
             parents_chain_index=protein14.parents_chain_index,
             remark=protein14.remark,
+            hetatom_positions=protein14.hetatom_positions,
+            hetatom_names=protein14.hetatom_names,
         )
 
     def to_protein27(self) -> Protein27:
@@ -1082,6 +1140,8 @@ class ProteinCATrace(ProteinBase):
             parents=protein_ca.parents,
             parents_chain_index=protein_ca.parents_chain_index,
             remark=protein_ca.remark,
+            hetatom_positions=protein_ca.hetatom_positions,
+            hetatom_names=protein_ca.hetatom_names,
         )
 
     def to_pdb(self) -> str:

proteome/visual.py
@@ -10,11 +10,17 @@ from proteome.pdb_utils import overwrite_b_factors
 def view_protein_with_bfactors(
     structure: "protein.ProteinBase",
     cmap: str,
+    show_sidechains: bool = True,
 ) -> py3Dmol.view:
-    band_edges = (50, 70, 90, 100)
+    band_edges = np.arange(50, 110, step=10)
 
     # Must be a 37 atom protein
     structure = structure.to_protein37()
+
+    # If no atoms beyond the fifth are present then
+    # we don't have sidechains to show
+    if not np.any(structure.atom_mask[:, 5:]):
+        show_sidechains = False
 
     # Get the colors from pyplot
     n = len(band_edges)
@@ -46,7 +52,8 @@ def view_protein_with_bfactors(
     color_map = {i: band_colors_hex[i] for i in range(len(band_edges))}
     style = {"cartoon": {"colorscheme": {"prop": "b", "map": color_map}}}
 
-    style["stick"] = {}
+    if show_sidechains:
+        style["stick"] = {}
 
     view.setStyle({"model": -1}, style)
     view.zoomTo()